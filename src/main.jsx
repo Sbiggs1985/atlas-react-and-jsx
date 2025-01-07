ab6d2dad51c70ee70df762d59b9fd4efe6ad816c
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2025-01-07 15:55:32 -0600

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'; //Responsible for rendering a React application in the browser.
+import App from './App'; // import the App component
 import "./index.css";
 
+const rootElement = document.getElementById('root');
+
 // Render the App component to the root element
+ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
+
